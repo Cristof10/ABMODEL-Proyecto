198a9707e8a340b92c4158108b5d22c1298c8249
Ness <nardy.cachipuendo@epn.edu.ec>
2024-01-02 16:00:02 -0500

    correción mensajes de error y raiz

--- Version-2/index.php
+++ Version-2/index.php
@@ -12,7 +12,7 @@
     $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
     // Verifica si la ruta de la solicitud es la raíz
-    if ($request_uri[0] == '/proyecto/ABMODEL-proyecto/Version-2/') {
+    if ($request_uri[0] == '/') {
         // En este caso, envía el archivo HTML inicial
         require 'pagina_inicial.php';
     } else {
